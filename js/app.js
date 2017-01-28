cde76bf7ce2fcbd96dc79d17eafc9ddd05fa3f69
Katie Winkler <katelynrwinkler@gmail.com>
2017-01-28 14:51:48 -0500

    timescale notes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
-//document.body.innerHTML = `<h1>${d3.version}</h1>`;
-var linearScale = d3.scaleLinear()
-	.domain([0,100])
-	.range([0,1])
-	.clamp(true);
+var timeScale = d3.scaleTime()
+	.domain([new Date(2016,0,1), new Date])
+	.range([0,100]);
 
-console.log(linearScale(-20));
-console.log(linearScale(0));
-console.log(linearScale(50));
-console.log(linearScale(100));
-console.log(linearScale(120));
+	console.log(timeScale(new Date(2016, 6, 4)));
+	console.log(timeScale(new Date(2016, 7, 29)));
 
-console.log(linearScale.invert(.5));
+	console.log(timeScale.invert(50));
