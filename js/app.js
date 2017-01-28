fe58c69b81eb6f076865fcb6772aedd09a773ab3
Katie Winkler <katelynrwinkler@gmail.com>
2017-01-28 17:21:30 -0500

    bar chart

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var margin = { top: 10, right: 15, bottom:50, left: 50}
+var margin = { top: 10, right: 3, bottom:70, left: 30}
 
 var width = 425 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
@@ -10,11 +10,15 @@ var svg = d3. select('.chart') //svg: scaleable vector graphic
 	.append('g')
 		.attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-svg.append('rect')
-	.attr('width', width)
-	.attr('height', height)
-	.style('fill', 'lightblue')
-	.style('stroke', 'green');
+var data = [ 
+	{ score: 63, subject: 'Math'},
+	{ score: 98, subject: 'Beer'},
+	{ score: 67, subject: 'Journalism'},
+	{ score: 25, subject: 'Football'},
+	{ score: 83, subject: 'Basketball'},
+	{ score: 90, subject: 'Michigan'},
+	{ score: 5, subject: 'Trump'},
+];
 
 var yScale = d3.scaleLinear()
 	.domain([0,100])
@@ -24,20 +28,31 @@ var yAxis = d3.axisLeft(yScale).ticks(5);
 
 svg.call(yAxis)
 
-var xScale = d3.scaleTime()
-	.domain([new Date(2016, 0, 1), new Date(2017, 0, 1)])
+var xScale = d3.scaleBand()
+	.paddingInner(0.1)
+	.paddingOuter(0.4)
+	.domain(data.map(d => d.subject))
 	.range([0, width]);
 
 var xAxis = d3.axisBottom(xScale)
 	.ticks(5)
 	.tickSizeOuter(15)
-	.tickPadding(15);
+	.tickPadding(10);
 
 svg 
 	.append('g')
 		.attr('transform', `translate(0, ${height})`)
-	.call(xAxis);
+	.call(xAxis)
+		.selectAll('text')
+		.style('text-anchor', 'start')
+		.attr('transform', 'rotate(45)');
 
-
-
+svg.selectAll('rect')
+	.data(data)
+	.enter()
+	.append('rect')
+	.attr('x', d => xScale(d.subject))
+	.attr('y', d => yScale(d.score))
+	.attr('width', d => xScale.bandwidth())
+	.attr('height', d => height - yScale(d.score));
 
