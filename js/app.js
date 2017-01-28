0bf3c6f363ad9c4322e2ef9d2cf7ed7060a628ad
Katie Winkler <katelynrwinkler@gmail.com>
2017-01-28 15:02:51 -0500

    quantizeScale

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-var timeScale = d3.scaleTime()
-	.domain([new Date(2016,0,1), new Date])
-	.range([0,100]);
+var quantizeScale = d3.scaleQuantize()
+	.domain([0,100])
+	.range(['red','green','white']);
 
-	console.log(timeScale(new Date(2016, 6, 4)));
-	console.log(timeScale(new Date(2016, 7, 29)));
+console.log(quantizeScale(30));
+console.log(quantizeScale(61));
+console.log(quantizeScale(90));
 
-	console.log(timeScale.invert(50));
+console.log(quantizeScale.invertExtent('white'));
