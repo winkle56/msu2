0a4d1f47f7add85058bca79bbbf89ca32895f863
Katie Winkler <katelynrwinkler@gmail.com>
2017-01-28 16:40:41 -0500

    graph notes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var margin = { top: 0, right: 0, bottom:50, left: 50}
+var margin = { top: 10, right: 15, bottom:50, left: 50}
 
 var width = 425 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
@@ -15,3 +15,29 @@ svg.append('rect')
 	.attr('height', height)
 	.style('fill', 'lightblue')
 	.style('stroke', 'green');
+
+var yScale = d3.scaleLinear()
+	.domain([0,100])
+	.range ([height, 0]);
+
+var yAxis = d3.axisLeft(yScale).ticks(5);
+
+svg.call(yAxis)
+
+var xScale = d3.scaleTime()
+	.domain([new Date(2016, 0, 1), new Date(2017, 0, 1)])
+	.range([0, width]);
+
+var xAxis = d3.axisBottom(xScale)
+	.ticks(5)
+	.tickSizeOuter(15)
+	.tickPadding(15);
+
+svg 
+	.append('g')
+		.attr('transform', `translate(0, ${height})`)
+	.call(xAxis);
+
+
+
+
